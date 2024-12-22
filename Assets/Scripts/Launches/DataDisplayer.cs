617d282447003e8777a8ae795c2d066976cf014a
sannie-beep <81695359+sannie-beep@users.noreply.github.com>
2024-12-22 20:52:21 +0800

    Clean code and display launch data
    - Clean up code and delete unnecessary classes
    - Display launch data of one launch ( yet to display all launches)
    - Set up scrollview and horizontal layout group)

---
+++ Assets/Scripts/Launches/DataDisplayer.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class DataDisplayer : MonoBehaviour
+{
+    
+    public GameObject launchPanel; // Prefab for launch data display object
+
+    private LaunchDatabase launchesData; // Launch database to be referenced
+    public LaunchDataLoader loader; // Loader which loads data using API
+
+
+    // Begin to wait for the data to be accessed and parsed
+    void Start()
+    {
+        StartCoroutine(WaitForDataAndDisplay());
+    }
+
+
+    IEnumerator WaitForDataAndDisplay()
+    {
+        // Wait till the get request has finished
+        yield return loader.LoadLaunchData("https://api.spacexdata.com/v4/launches");
+
+        // Get the launch data
+        launchesData = loader.getLaunchData();
+
+        if (launchesData.launches == null || launchesData.launches.Count == 0)
+        {
+            Debug.LogError("No launches found in the data!");
+        }
+
+        // Display the launch information
+        displayLaunchInfo();
+    }
+
+
+
+    void displayLaunchInfo()
+    {     
+    // Find Name TextMeshProUGUI
+    TextMeshProUGUI nameTMP = launchPanel.transform.Find("NamePanel/Name")?.GetComponent<TextMeshProUGUI>();
+
+    // Find Payloads TextMeshProUGUI
+    TextMeshProUGUI payloadsTMP = launchPanel.transform.Find("PayloadsPanel/Payloads")?.GetComponent<TextMeshProUGUI>();
+
+    // Set data
+    setName(nameTMP);
+    setPayloads(payloadsTMP);
+
+    }
+
+    // Access and display launch name
+    void setName(TextMeshProUGUI nameTMP)
+    {
+        nameTMP.text += launchesData.launches[5].name;
+    }
+    
+    // Access and display number of payloads
+    void setPayloads(TextMeshProUGUI payloadsTMP)
+    {
+        String[] payLoadsList = launchesData.launches[5].payloads;
+        int numPayloads = payLoadsList.Count();
+        payloadsTMP.text += Convert.ToString(numPayloads);
+    }
+}
--- Assets/Scripts/Launches/LaunchDataLoader.cs
+++ Assets/Scripts/Launches/LaunchDataLoader.cs
@@ -1,72 +1,76 @@
 using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Runtime.ConstrainedExecution;
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.Networking;
 using System.IO;
 
 public class LaunchDataLoader : MonoBehaviour
 {
+    public LaunchDatabase loadedLaunchData; // To store launch data and access it to be displayed by DataDisplayer
 
-    // Start is called before the first frame update
-    public LaunchDatabase loadedLaunchData;
-    void Start()
-    {
-        loadLaunchData("https://api.spacexdata.com/v4/launches");
-    }
-
-
-    public void loadLaunchData(String url)
-    {
-        StartCoroutine(GetRequest(url)); 
-        //System.IO.File.WriteAllText(Application.persistentDataPath + "/LaunchData.json", launchDataJson.toJson());
-    }
-
-    IEnumerator GetRequest(String url)
+    // Loads launch data : executes Get Request and invokes parsing method
+    public IEnumerator LoadLaunchData(String url)
     {
         UnityWebRequest launchDataJson = UnityWebRequest.Get(url);
         yield return launchDataJson.SendWebRequest();
         
+        // If request is successful
         if (launchDataJson.result == UnityWebRequest.Result.Success){
+
+            // Save as json in resources folder (in case needed)
             string jsonData = launchDataJson.downloadHandler.text;
             string filePath = Path.Combine(Application.dataPath, "Resources/LaunchData.json");
-            //LaunchDatabase launchData = JsonUtility.FromJson<LaunchDatabase>(jsonData);
-            // Save the JSON string to the file
             File.WriteAllText(filePath, jsonData);
-           // int count = launchData.launches.Count;
-
-           ParseLaunchData(jsonData);
+            
+            // Parse json data into LaunchDatabase object
+            ParseLaunchData(jsonData);
            
-           // System.IO.File.WriteAllText(Application.persistentDataPath + "/LaunchData.json", jsonData);
         }
-        else{
+        // Else display error message
+        else {
             Debug.LogError("Unable to access launch data.");
         }
 
     }
 
- void ParseLaunchData(string json)
-{
-        // Wrap the JSON array into a JSON object
-        string wrappedJson = $"{{\"launches\": {json}}}";
-        
-        // Parse the wrapped JSON
-        LaunchDatabase launchDataList = JsonUtility.FromJson<LaunchDatabase>(wrappedJson);
-        
-        //assign it to public variable
-        this.loadedLaunchData = launchDataList;
+    // Parses json accessed into LaunchDatabase object
+    void ParseLaunchData(string json)
+    {
+            // Wrap the JSON array into a JSON object
+            string wrappedJson = $"{{\"launches\": {json}}}";
+            
+            // Parse the wrapped JSON
+            LaunchDatabase launchDataList = JsonUtility.FromJson<LaunchDatabase>(wrappedJson);
+            
+            //Assign it to public variable
+            this.loadedLaunchData = launchDataList;
 
-        if (launchDataList.launches != null){
-             foreach (var launch in launchDataList.launches)
-            {
-                String name = launch.name;
-                Debug.Log(name);
-            }   
+            // Check if is null
+            if (this.loadedLaunchData.launches != null){
+                foreach (var launch in this.loadedLaunchData.launches)
+                {
+                    String name = launch.name;
+                    //Debug.Log(name); --> for debugging
+                }   
+            }
+            else {
+                Debug.LogError("Launch Data not loaded.");
+            }
+             
+            //Debug.Log("Launch Data loaded"); --> for debugging
+    }
+
+    // Returns the launch data if it is not null
+    public LaunchDatabase getLaunchData(){
+
+        // make sure it is loaded already
+        if (this.loadedLaunchData == null || this.loadedLaunchData.launches == null)
+        {
+            Debug.Log("Not loaded yet, please wait...");
+            return null;
         }
 
-
-    //return launchDataList; 
-}
+        Debug.Log("Launch data accessed.");
+        return this.loadedLaunchData;
+    }
 }
